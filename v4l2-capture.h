d8ba9ee96f0baccd0b0ddf8ab84cc25b6d6a0b77
Oleh Kuzhylnyi <o.kuzhyl@gmail.com>
2021-07-10 21:38:40 +0300

    global code refactoring

--- v4l2-capture.h
+++ v4l2-capture.h
@@ -2,27 +2,26 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <pthread.h>
 #include <linux/videodev2.h>
 
-#define V4L2_DEFAULT_VIDEO_DEVICE "/dev/video0"
+#define V4L2_REQUESTED_BUFFERS_NUM	10
+#define V4L2_REQUESTED_PLANES_NUM	1
 
-#define V4L2_REQUESTED_BUFFERS_NUM      4
-#define V4L2_REQUESTED_PLANES_NUM       1
+#define V4L2_WIDTH_DEFAULT		640
+#define V4L2_HEIGHT_DEFAULT		480
+#define V4L2_PIXEL_FORMAT_DEFAULT	V4L2_PIX_FMT_YUYV
+#define V4L2_FIELD_DEFAULT		V4L2_FIELD_ANY
 
-#define V4L2_WIDTH_DEFAULT              640
-#define V4L2_HEIGHT_DEFAULT             480
-#define V4L2_PIXEL_FORMAT_DEFAULT       V4L2_PIX_FMT_YUYV
-#define V4L2_FIELD_DEFAULT              V4L2_FIELD_ANY
-
-struct frame {
-	void    **head;          // Beginning of valid buffer
-	size_t  *length;         // Size of whole buffer
+struct v4l2_frame {
+	void    **head;          // Beginning of valid frame buffer
+	size_t  *length;         // Length of whole frame buffer
 };
 
 struct v4l2_frame_buffer {
-	struct  frame f;
-	size_t  index;          // Id number of the valid buffer
-	size_t  bytes_used;     // Number of used bytes, may be less then page_size
+	struct  v4l2_frame f;
+	size_t  index;          // Index of a valid buffer
+	size_t  bytes_used;     // Number of used bytes to write frame, may be less then page_size
 };
 
 struct v4l2_camera_params {
@@ -34,13 +33,29 @@ struct v4l2_camera_params {
 };
 
 struct v4l2_camera {
+	/* Descriptor for /dev/videoN node */
 	int vfd;
+
+	/* Frame buffers number */
+	size_t fb_num;
+
+	/* Number of planes */
+	size_t mplane_num;
+
+	/* Pointer to frame buffers */
 	struct v4l2_frame_buffer *fb;
+
 	struct v4l2_camera_params params;
+
+	/* Indicated that buffer is ready to be fetched */
+	bool v4l2_is_frame_ready;
+
+	/* Lock for v4l2_is_frame_ready */
+	pthread_mutex_t c_lock;
 };
 
-extern bool v4l2_frame_ready;
 extern bool v4l2_is_polling;
 
-int v4l2_try_set_format(int *vfd);
-struct v4l2_camera *v4l2_start_video_capturing(const char *video_dev);
+struct v4l2_camera *v4l2_start_video_capturing(const char *video_dev,
+					       size_t w, size_t h, size_t pix_fmt,
+					       size_t buf_num, size_t mplane_num);
